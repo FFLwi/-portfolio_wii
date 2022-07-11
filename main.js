7393e648f5a491b66ec4479ac07d2868f3fbf957
FFLwi <wi04707@gmail.com>
2022-07-11 16:31:49 +0900

    js:navbar

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  //console.log(window.scrollY);
+    //console.log(navbarHeight);
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+
+//Handle scrolling when tapping onthe navbar menu
+const navbarMeun =document.querySelector('.navbar__menu');
+navbarMeun.addEventListener('click',(event)=>{
+    console.log(event.target.dataset.link);
+    const target =event.target;
+    const link=target.dataset.link;
+    if(link== null){
+      return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
+
+
